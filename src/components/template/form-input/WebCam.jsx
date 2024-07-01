7124da02a66b18aea4f5a705ac9a058c3c15419b
Snakesystem <ir15y4hh@gmail.com>
2024-07-01 10:17:38 +0700

    Update kamre

--- src/components/template/form-input/WebCam.jsx
+++ src/components/template/form-input/WebCam.jsx
@@ -5,7 +5,7 @@ import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import { useSweetAlert } from '../../../hooks/useSweetAlert';
 
-const VideoCam = ({webcamRef, videoConstraints}) => {
+const VideoCam = ({webcamRef, videoConstraints, toggleFacingMode}) => {
     return (
         <div className="swal-cover">
             <div className="cover-top"></div>
@@ -21,7 +21,7 @@ const VideoCam = ({webcamRef, videoConstraints}) => {
                     <button type="button" className="btn btn-primary me-2 webcam-button" >
                     <i className="bi bi-camera"></i>
                     </button>
-                    <button type="button" className="btn btn-secondary webcam-button">
+                    <button type="button" className="btn btn-secondary webcam-button" onClick={toggleFacingMode}>
                     <i className="bi bi-arrow-repeat"></i>
                     </button>
                     <button type="button" className="btn btn-danger webcam-button">
@@ -39,29 +39,36 @@ export default function WebCam() {
     const webcamRef = useRef(null);
     const [capturedImage, setCapturedImage] = useState(null);
     const [facingMode, setFacingMode] = useState('user');
-const [showPreview, setShowPreview] = useState(false);
+    const [showPreview, setShowPreview] = useState(false);
     const MySwal = withReactContent(Swal);
     const { showAlert } = useSweetAlert();
 
     const { control, setValue } = useFormContext();
 
     const videoConstraints = {
-        width: 480,
-        height: 640,
+        width: window.innerWidth,
+        height: window.innerHeight,
         facingMode,
-      };
+    };
+
+    const toggleFacingMode = () => {
+      setFacingMode((prevMode) => (prevMode === 'user' ? 'environment' : 'user'));
+    };
 
     const openCamera = () => {
         showAlert({
-            html: <VideoCam webcamRef={webcamRef} videoConstraints={videoConstraints}/>,
+            html: <VideoCam webcamRef={webcamRef} videoConstraints={videoConstraints} toggleFacingMode={toggleFacingMode}/>,
             allowOutsideClick: false,
             showConfirmButton: false,
             showCancelButton: false
         })
     }
+
   return (
     <div className="form-group mb-3">
       <button type="button" className='btn btn-primary' onClick={openCamera}><i className="bi bi-camera"></i></button>
+      {/* <VideoCam webcamRef={webcamRef} videoConstraints={videoConstraints}/> */}
+      
       <Controller
         name="webcam"
         control={control}
